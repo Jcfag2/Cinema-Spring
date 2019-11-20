6981ef942143d6c9139f8c3712ac688752d12ae2
jcfag2 <j.fagothey@esff.fr>
2019-11-20 11:49:28 +0100

    màj

--- src/test/java/fr/gtm/cinema/CinemaSpringApplicationTests.java
+++ src/test/java/fr/gtm/cinema/CinemaSpringApplicationTests.java
@@ -25,22 +25,27 @@ class CinemaSpringApplicationTests {
 	void getAll() {
 		List<Film> films = repo.findAll();
 		assertTrue(films.size()>0);
-		System.out.println("=================================");
+		System.out.println(" ");
+		System.out.println("====================================================================================================================================");
+		System.out.println(" ");
 		for(Film f : films) {
-			System.out.println(f);
+			System.out.println("\u001B[33m"+"Film: "+"\u001B[34m"+f.getTitre()+"\u001B[33m"+"\nRéalisé par: "+"\u001B[34m"+f.getRealisateur()+"\u001B[33m"+"\nSortie le : "+"\u001B[34m"+f.getDateSortie()+"\u001B[33m"+"\nD'une durée de: "+"\u001B[34m"+f.getDuree()+" minutes.\u001B[0m");
+			System.out.println(" ");
 		}
-		System.out.println("=================================");
+		System.out.println("====================================================================================================================================");
+		System.out.println(" ");
 	}
 	
 	@Test
 	void getFilmEtActeursbyId() {
-		Film film = repo.getFilmEtActeursById(1);
-		assertTrue(film.getRole().size()==2);
-		System.out.println("=================================");
-		System.out.println(film);
-		System.out.println(film.getRole());
-		System.out.println("=================================");
-		
+		Film f = repo.getFilmEtActeursById(1);
+		assertTrue(f.getRole().size()==2);
+		System.out.println(" ");
+		System.out.println("====================================================================================================================================");
+		System.out.println("\u001B[33m"+"Film: "+"\u001B[34m"+f.getTitre()+"\u001B[33m"+"\nRéalisé par: "+"\u001B[34m"+f.getRealisateur()+"\u001B[33m"+"\nSortie le : "+"\u001B[34m"+f.getDateSortie()+"\u001B[33m"+"\nD'une durée de: "+"\u001B[34m"+f.getDuree()+" minutes.\u001B[0m");
+		System.out.println("\u001B[36m"+f.getRole()+"\u001B[0m");
+		System.out.println("====================================================================================================================================");
+		System.out.println(" ");
 	}
 
 }
