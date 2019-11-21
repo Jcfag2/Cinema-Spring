d498b528c86324be526de80768c29a41ffb9fd97
jcfag2 <j.fagothey@esff.fr>
2019-11-21 14:12:54 +0100

    yeay!

--- src/main/java/fr/gtm/cinema/CinemaSpringApplication.java
+++ src/main/java/fr/gtm/cinema/CinemaSpringApplication.java
@@ -3,16 +3,43 @@ package fr.gtm.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableAsync
+@EnableSwagger2
 public class CinemaSpringApplication {
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaSpringApplication.class, args);
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.basePackage("fr.gtm.cinema.rest"))
+					.build()
+					.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("REST API de gestion de films")
+				.contact(new Contact("Erwan", "http://gros.penis.fr", "e.soubeyrand@gros.penis.fr"))
+				.version("0.alpha")
+				.build();
 	}
 
 }
